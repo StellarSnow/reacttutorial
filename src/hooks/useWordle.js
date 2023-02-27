d86d7de074360ece988a26615ad6bae8eded7217
Dane Dwarka <danedwarka@hotmail.com>
2023-02-27 10:19:47 -0600

    Part 7 - add new guesses

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 const useWordle = (solution) => {
     const [turn, setTurn] = useState(0)
     const [currentGuess, setCurrentGuess] = useState('')
-    const [guesses, setGuesses] = useState([])
+    const [guesses, setGuesses] = useState([...Array(6)])
     const [history, setHistory] = useState([])
     const [isCorrect, setIsCorrect] = useState(false);
 
@@ -30,8 +30,27 @@ const useWordle = (solution) => {
         return formattedGuess
     }
 
-    const addNewGuess = () => {
+    const addNewGuess = (formattedGuess) => {
+        if (currentGuess === solution) {
+            setIsCorrect(true)
+        }
 
+        setGuesses((prevGuesses) => {
+            let newGuesses = [...prevGuesses]
+            newGuesses[turn] = formattedGuess
+
+            return newGuesses
+        })
+
+        setHistory((prevHistory) => {
+            return [...prevHistory, currentGuess]
+        })
+
+        setTurn((prevTurn) => {
+            return prevTurn + 1
+        })
+
+        setCurrentGuess('')
     }
 
     const handleKeyup = ({ key }) => {
@@ -52,7 +71,7 @@ const useWordle = (solution) => {
             }
 
             const formatted = formatGuess()
-            console.log(formatted)
+            addNewGuess(formatted)
         }
 
         if (key === 'Backspace') {

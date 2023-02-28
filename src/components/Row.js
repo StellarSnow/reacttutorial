2bcd94c8dd78c7cd2f11378cc96ffe5d7c381741
Dane Dwarka <danedwarka@hotmail.com>
2023-02-27 21:36:37 -0600

    Part 10 - Show the current guess

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Row({ guess }) {
+export default function Row({ guess, currentGuess }) {
 
   if (guess) {
     return (
@@ -11,6 +11,21 @@ export default function Row({ guess }) {
       </div>
     )
   } 
+
+  if (currentGuess) {
+    let letters = currentGuess.split('')
+
+    return (
+      <div className="row current">
+        {letters.map((letter, index) => (
+          <div key={index} className="filled">{letter}</div>
+        ))}
+        {[...Array(5 - letters.length)].map((value, index) => (
+          <div key={index}></div>
+        ))}
+      </div>
+    )
+  }
 
   return (
     <div className="row">
